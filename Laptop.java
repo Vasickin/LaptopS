9ce7d21319d776cacd797991290e0e3cd1f99ab4
Lex <vasickin@mail.ru>
2024-05-01 03:08:33 +0300

    Class definition

---
+++ Laptop.java
@@ -0,0 +1,13 @@
+package LaptopStore;
+
+import java.util.Objects;
+
+public class Laptop {
+    private String brabd;
+    private int ram;
+    private int storrageCapacity;
+    private String operationSystem;
+    private int displaySize;
+    private String color;
+
+}
